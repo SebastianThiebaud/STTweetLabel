074c96759a7cde9e44a229fefc26a55357726f7a
Sebastien THIEBAUD <sebastien.thiebaud@supinfo.com>
2012-12-15 20:45:33 -0800

    Initial Commit

---
+++ STTweetLabel/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  STTweetLabel
+//
+//  Created by Sebastien Thiebaud on 12/15/12.
+//  Copyright (c) 2012 Sebastien Thiebaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ STTweetLabel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  STTweetLabel
+//
+//  Created by Sebastien Thiebaud on 12/15/12.
+//  Copyright (c) 2012 Sebastien Thiebaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+    }
+}
