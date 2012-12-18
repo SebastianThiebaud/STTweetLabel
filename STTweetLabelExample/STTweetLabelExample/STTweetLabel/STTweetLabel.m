f3e5b3e220f488b33c637e5a22051d4b191ac155
Giacomo Saccardo <gsaccardo@gmail.com>
2012-12-18 14:38:00 +0100

    Add three optional methods for different kind of url, remove old required methos

--- STTweetLabelExample/STTweetLabelExample/STTweetLabel/STTweetLabel.m
+++ STTweetLabelExample/STTweetLabelExample/STTweetLabel/STTweetLabel.m
@@ -123,11 +123,38 @@
     [touchLocations enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
         CGRect touchZone = [obj CGRectValue];
 
-        // If a touchable word is found
-        if (CGRectContainsPoint(touchZone, touchPoint) && [_delegate respondsToSelector:@selector(tweetLinkClicked:)])
-        {
-            [_delegate tweetLinkClicked:[touchWords objectAtIndex:idx]];
+        if (CGRectContainsPoint(touchZone, touchPoint)) {
+            //A touchable word is found
+            
+            NSString *url = [touchWords objectAtIndex:idx];
+            
+            if ([[touchWords objectAtIndex:idx] hasPrefix:@"@"]) {
+                
+                //Twitter account clicked
+                if ([_delegate respondsToSelector:@selector(twitterAccountClicked:)]) {
+                    [_delegate twitterAccountClicked:url];
+                }
+                
+            }
+            else if ([[touchWords objectAtIndex:idx] hasPrefix:@"#"]) {
+                
+                //Twitter hashtag clicked
+                if ([_delegate respondsToSelector:@selector(twitterHashtagClicked:)]) {
+                    [_delegate twitterHashtagClicked:url];
+                }
+                
+            }
+            else if ([[touchWords objectAtIndex:idx] hasPrefix:@"http"]) {
+                
+                //Twitter hashtag clicked
+                if ([_delegate respondsToSelector:@selector(websiteClicked:)]) {
+                    [_delegate websiteClicked:url];
+                }
+                
+            }
+            
         }
+        
     }];
 }
 
